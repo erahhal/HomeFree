4e63329d784a2370fe75131d8d999055165bb617
Ellis Rahhal <erahhal@netflix.com>
2023-12-09 15:57:38 -0800

    Disable DNS for dnsmasq, enabled for Unbound

--- TODOS.md
+++ TODOS.md
@@ -1,19 +1,28 @@
 TODOS
 =====
 
+* setup VLANs
+  * https://wiki.nftables.org/wiki-nftables/index.php/Main_Page
+  * https://serverfault.com/questions/858556/transparent-firewall-with-nftables-and-vlans
+  * https://serverfault.com/questions/1057819/route-untagged-vlan-to-a-tagged-vlan-with-nftables
+* setup ipv6
+* Make a flake that sets up host machine for dev
+* Figure out how to use host id_rsa.pub in build, rather than hard-coded key
+* Determine if there are any problems with disabling "wait for network" to get rid of error
 * Look into microvm instead of qemu, which has been difficult to work with
   * https://github.com/astro/microvm.nix
-* Make a flake that sets up host machine for dev
+* setup DNSSEC for dnsmasq
+  * https://blog.josefsson.org/2015/10/26/combining-dnsmasq-and-unbound/
+
+### DONE
+
 * Setup host so default network virbr0 starts at boot
   * currently need to us "sudo virsh net-start --network default"
 * Does hardware-configuration.nix root disk device need to be updated with each build?
-* Get VM starting from command line - export XML?
+* Get VM starting from command line
 * Figure out way to setup SSH without needing to lookup IP in virt-manager
   * Use user network, map SSH to unused port
   * https://unix.stackexchange.com/questions/489891/qemu-access-guest-vm-from-the-host-machine
   * better though would be to add hostname, e.g. homefree-vm
-* Figure out how to use host id_rsa.pub in build, rather than hard-coded key
-* Disable wait for network to get rid of error
-  * What are the ramifications?
 * Share folder automatically setup by qemu xml?
   * https://www.debugpoint.com/share-folder-virt-manager/
--- profiles/router.nix
+++ profiles/router.nix
@@ -217,28 +217,48 @@ in
     '';
   };
 
-  # @TODO: Look at Unbound instead
-  #   https://github.com/MayNiklas/nixos-adblock-unbound
-
   services.dnsmasq = {
     enable = true;
 
     settings = {
       ## @TODO
       ## @WARNING - changes to this do not clear out old entries from /etc/dnsmasq-conf.conf
+
+      ## Only DHCP server on network
+      dhcp-authoritative = true;
+
+      ## Enable Router Advertising for ipv6
+      enable-ra = true;
+
+      ## DNS servers to pass to clients
       server = dns-servers;
+
+      ## Which interfaces to bind to
       interface = [
         # "${lan-interface}.${builtins.toString vlan-lan-id}"
         # "${lan-interface}.${builtins.toString vlan-iot-id}"
         # "${lan-interface}.${builtins.toString vlan-guest-id}"
         lan-interface
       ];
+
+      ## IP ranges to hand out
       dhcp-range = [
         # "lan,10.1.1.100,10.1.1.254,255.255.255.0,8h"
         # "iot,10.2.1.100,10.2.1.254,255.255.255.0,8h"
         # "guest,10.3.1.100,10.3.1.254,255.255.255.0,8h"
         "${lan-interface},10.1.1.100,10.1.1.254,255.255.255.0,8h"
       ];
+
+      ## Disable DNS
+      port = 0;
+
+      ## Additional DHCP options
+      dhcp-option = [
+        "option6:dns-server,[::]"  # @TODO: point this at Unbound when ipv6 is setup
+        "option:dns-server,10.1.1.1"
+      ];
+
+      cache-size = 500;
     };
   };
 
@@ -248,6 +268,53 @@ in
 
   ## @TODO - Setup Unbound
   ## See: https://blog.josefsson.org/2015/10/26/combining-dnsmasq-and-unbound/
+
+  services.unbound = {
+    enable = true;
+
+    user = "root";
+
+    resolveLocalQueries = true;
+
+    settings = {
+      server = {
+        interface = [
+          "127.0.0.1"
+          "::1"
+          "10.1.1.1"
+        ];
+        access-control = [
+          "127.0.0.1/8 allow"
+          "::1 allow"
+          "10.1.1.1/8 allow"
+          # @TODO: need ipv6 address
+        ];
+        outgoing-interface = [
+          ## @TODO: should be WAN IP - how to get this automatically?
+          "10.0.2.15"
+          # @TODO: need ipv6 address
+        ];
+      };
+      forward-zone = [
+        {
+          name = ".";
+          forward-addr = [
+            "1.1.1.1@853#cloudflare-dns.com"
+            "1.0.0.1@853#cloudflare-dns.com"
+          ];
+          forward-tls-upstream = "yes";
+        }
+        # {
+        #   name = "example.org.";
+        #   forward-addr = [
+        #     "1.1.1.1@853#cloudflare-dns.com"
+        #     "1.0.0.1@853#cloudflare-dns.com"
+        #   ];
+        # }
+      ];
+      remote-control.control-enable = true;
+    };
+  };
 
   #-----------------------------------------------------------------------------------------------------
   # Service Discovery
