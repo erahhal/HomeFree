29877e469d0aa3c45ce4d92356a8205b5521e1b4
Ellis Rahhal <erahhal@netflix.com>
2024-04-25 23:49:16 -0700

    authentik ldap working; home-assistant auth with LDAP working

---
+++ profiles/home-assistant/ldap.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+let
+  ldap-auth-sh = pkgs.callPackage ./ldap-auth-sh.nix;
+in
+{
+  services.home-assistant.config.homeassistant.auth_providers = [
+    {
+      type = "command_line";
+      command = "${ldap-auth-sh}/bin/ldap-auth.sh";
+      meta = true;
+    }
+  ];
+}
