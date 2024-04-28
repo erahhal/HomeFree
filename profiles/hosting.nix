35b63eaa458c6a273f60d0bd8dbc6781491e4dab
Ellis Rahhal <erahhal@netflix.com>
2024-04-28 16:12:46 -0700

    fixed broken ldap import; temporarily disabled http to https caddy redirect

--- profiles/hosting.nix
+++ profiles/hosting.nix
@@ -1,5 +1,9 @@
-{ pkgs, ... }:
-
+{ config, pkgs, ... }:
+let
+  hostConfig = ''
+    respond "Hello, world! I am being accessed from {scheme}."
+  '';
+in
 {
   imports = [
     ../apps/radicale.nix
@@ -11,13 +15,20 @@
     ## reload config while running instead of restarting. true by default.
     enableReload = true;
 
-    virtualHosts."localhost" = {
-      extraConfig = ''
-        respond "Hello, my world!"
+    ## With both http and https set, caddy won't redirect http to https
+    ## REMOVE THIS IN PROD
+    virtualHosts."http://localhost, https://localhost" = {
+      # Nix config mangles the log name, so set it manually
+      logFormat = ''
+        output file ${config.services.caddy.logDir}/access-localhost.log
       '';
+      extraConfig = hostConfig;
     };
 
     virtualHosts."http://radicale.homefree.lan" = {
+      logFormat = ''
+        output file ${config.services.caddy.logDir}/access-radicale.log
+      '';
       extraConfig = ''
         reverse_proxy :5232
       '';
