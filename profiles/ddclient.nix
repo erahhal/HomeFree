9d6d46229a3978681c523e97974605c1861f0e95
Ellis Rahhal <erahhal@netflix.com>
2024-10-26 14:41:16 -0700

    updated ddclient config

--- profiles/ddclient.nix
+++ profiles/ddclient.nix
@@ -5,15 +5,26 @@
   # Dynamic DNS
   #-----------------------------------------------------------------------------------------------------
 
-  # @TODO: https://discourse.nixos.org/t/ddclient-options/20935
   services.ddclient = {
     enable = true;
     interval = "10m";
-    # protocol = "zoneedit1";
-    # username = "erahhal";
-    # zone = "homefree.host";
-    # passwordFile = config.age.secrets.ddclient.path;
-    # verbose = true;
-    configFile = config.age.secrets.ddclient-conf.path;
+    protocol = "hetzner";
+    username = "erahhal";
+    zone = "homefree.host";
+    domains = [ "*" "www" "dev" ];
+    passwordFile = "/run/secrets/ddclient/ddclient-password";
+    use = "web, web=ipinfo.io/ip";
+    verbose = true;
+  };
+
+  sops.secrets = {
+    "ddclient/ddclient-password" = {
+      format = "yaml";
+      sopsFile = ../secrets/ddclient.yaml;
+
+      owner = "homefree";
+      path = "/run/secrets/ddclient/ddclient-password";
+      restartUnits = [ "ddclient.service" ];
+    };
   };
 }
