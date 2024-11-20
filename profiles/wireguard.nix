282bf47dc0ec8106ee604cd0dbbdd9ba453cc69b
Ellis Rahhal <ellis@rahh.al>
2024-11-20 01:21:27 -0800

    wireguard working with nftables

--- profiles/wireguard.nix
+++ profiles/wireguard.nix
@@ -20,20 +20,6 @@ in
       # The port that WireGuard listens to. Must be accessible by the client.
       listenPort = listenPort;
 
-      # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
-      # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
-      postSetup = ''
-        # ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 192.168.2.0/24 -o ${config.homefree.network.wan-interface} -j MASQUERADE
-        ${pkgs.nftables}/bin/nft add rule inet nat postrouting oifname "${config.homefree.network.wan-interface}" ip saddr 192.168.2.0/24 counter masquerade
-      '';
-
-      # This undoes the above command
-      postShutdown = ''
-        # ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 192.168.2.0/24 -o ${config.homefree.network.wan-interface} -j MASQUERADE
-        HANDLE=$(${pkgs.nftables}/bin/nft -a list chain inet nat postrouting | ${pkgs.gnugrep}/bin/grep "192.168.2.0" | ${pkgs.gawk}/bin/awk '/# handle [0-9]+/ {print $NF}')
-        ${pkgs.nftables}/bin/nft delete rule inet nat postrouting handle $HANDLE
-      '';
-
       # Path to the private key file.
       #
       # Note: The private key can also be included inline via the privateKey option,
