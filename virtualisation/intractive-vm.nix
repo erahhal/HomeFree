50cb17b8c3c00ae4c215a825acc8879661ddf963
Ellis Rahhal <ellis@rahh.al>
2024-12-24 18:17:21 -0800

    added stub vm file

---
+++ virtualisation/intractive-vm.nix
@@ -0,0 +1,7 @@
+{ ... }:
+{
+  ## Reference: https://github.com/nix-community/disko/blob/master/docs/interactive-vm.md
+  ## This allows users to run their own system config in a VM.
+  ## TODO: Add example/default config here to run in VM
+  ##       OR: checkout sample-config repo and run that in a VM, to make sure it works
+}
